3049e0d5a4dfb0696323e69ba9612712ace848cb
Naim3755 <muhaiminul3755@gmail.com>
2024-01-17 14:23:05 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/dashboardwithspinners/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dashboardwithspinners;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
